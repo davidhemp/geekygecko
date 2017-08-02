64af31c9fb4fefec974e5ba4d5606f617b1becd2
david <cyber_veio@hotmail.com>
2017-08-02 23:32:53 +0100

    Add Bootstrap framework

--- index.php
+++ index.php
@@ -4,9 +4,15 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem("templates");
 $twig = new Twig_Environment($loader);
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="static/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
